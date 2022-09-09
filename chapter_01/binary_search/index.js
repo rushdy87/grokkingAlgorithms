77d281784e2ffe849387af784b2be4790179df99
“rushdy87” <48268026+m7md7sam@users.noreply.github.com>
2022-09-09 05:14:00 +0300

    chapter_01

---
+++ chapter_01/binary_search/index.js
@@ -0,0 +1,23 @@
+const binary_search = (list, item) => {
+  let low = 0;
+  let high = list.length - 1;
+  while (low <= high) {
+    let mid = Math.floor((low + high) / 2);
+    let guess = list[mid];
+
+    if (guess === item) {
+      return mid;
+    }
+    if (guess > item) {
+      high = mid - 1;
+    } else {
+      low = mid + 1;
+    }
+  }
+  return null;
+};
+
+const list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+console.log(binary_search(list, 5));
+console.log(binary_search(list, 25));
